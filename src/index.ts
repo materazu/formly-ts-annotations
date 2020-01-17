c32968c28f2e806f265fce53f7fe15fc720501d0
Julien Moulin <julien@blueanchor.io>
2020-01-17 11:17:55 +0100

    Fixes

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,3 @@
-// import { FormlyConfig } from './lib/FormlyConfig';
-
-// import { GetFormlyConfiguration } from './lib/GetFormlyConfiguration';
-
-// class Demo {
-//   @FormlyConfig({
-//     type: 'input',
-//       templateOptions: {
-//         label: 'Title',
-//         placeholder: 'Title of the training',
-//         required: true,
-//       },
-//   })
-//   public title: any;
-
-//   @FormlyConfig({
-//     type: 'textarea',
-//     templateOptions: {
-//       label: 'Description',
-//       placeholder: 'Description',
-//       required: true,
-//       rows: 10,
-//     },
-//   })
-//   public description;
-// }
-
-// console.log(GetFormlyConfiguration(Demo));
-
 export * from './lib/FormlyConfig';
 export * from './lib/GetFormlyConfiguration';
+export * from './interfaces/formly.field.config';
--- src/lib/GetFormlyConfiguration.ts
+++ src/lib/GetFormlyConfiguration.ts
@@ -3,8 +3,8 @@ import 'reflect-metadata';
 import { FORMLY_CONFIG_KEY } from './constants';
 
 export function GetFormlyConfiguration(entity) {
-  const formlyConfig = [];
   const entityInstance = new entity();
+  const formlyConfig = [];
   const configs = Reflect.getMetadata(FORMLY_CONFIG_KEY, entityInstance);
 
   Object.keys(configs).forEach(key => {
